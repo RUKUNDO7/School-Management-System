01bf26d3f7a4af22c42fdfd981aa148c39a9d79f
Rukundo <gihozoRukundobenise@gmail.com>
2026-03-16 11:30:13 +0200

    Added fee repository

---
+++ smart-school-hub/src/main/java/com/smartschoolhub/repository/FeeRepository.java
@@ -0,0 +1,10 @@
+package com.smartschoolhub.repository;
+
+import com.smartschoolhub.domain.Fee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FeeRepository extends JpaRepository<Fee, Long> {
+    List<Fee> findByStudentId(Long studentId);
+}
