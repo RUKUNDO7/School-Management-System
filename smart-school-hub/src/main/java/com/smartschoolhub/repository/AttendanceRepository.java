8b5d68bc4b2ce163fc0bc874715dc227fd3487c0
Rukundo <gihozoRukundobenise@gmail.com>
2026-03-16 11:29:16 +0200

    Added attendance repository

---
+++ smart-school-hub/src/main/java/com/smartschoolhub/repository/AttendanceRepository.java
@@ -0,0 +1,12 @@
+package com.smartschoolhub.repository;
+
+import com.smartschoolhub.domain.Attendance;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
+    List<Attendance> findByStudentId(Long studentId);
+    List<Attendance> findByDate(LocalDate date);
+}
