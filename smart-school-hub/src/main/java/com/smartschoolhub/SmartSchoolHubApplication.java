c40a67a0f8967f7f559d375fc7df1534fca56533
Rukundo <gihozoRukundobenise@gmail.com>
2026-03-18 20:58:56 +0200

    Modified backend entry point

--- smart-school-hub/src/main/java/com/smartschoolhub/SmartSchoolHubApplication.java
+++ smart-school-hub/src/main/java/com/smartschoolhub/SmartSchoolHubApplication.java
@@ -1,11 +1,18 @@
 package com.smartschoolhub;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartSchoolHubApplication {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> {
+            if (System.getProperty(entry.getKey()) == null) {
+                System.setProperty(entry.getKey(), entry.getValue());
+            }
+        });
         SpringApplication.run(SmartSchoolHubApplication.class, args);
     }
 }
